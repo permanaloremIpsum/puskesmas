3d305c55670ea14d8903f317a7d2812382f4b7c6
permanaloremIpsum <dpermana350@gmail.com>
2019-06-30 12:37:38 +0700

    show pasien

--- puskesmas.cpp
+++ puskesmas.cpp
@@ -36,132 +36,288 @@ void add_pasien(){
 	// antrean_periksa.akhir++;
 }
 
+int kolom[] = {25, 35};
+int baris[] = {5, 25};
+int value = 7;
+
 void show_antrean(){
-	for (int i = 0; i < psn.count; ++i)
+	system("cls");
+	xc::color(10, 0);
+	xc::xy(25,3); cout << "Antrian Pasien : ";
+	xc::color(7, 0);
+	string header[] = {"No", "Nama Pasien"};
+	for(auto r = 0; r < 2; ++r)
 	{
-		cout << "Nama : " << antrean_periksa.data[antrean_periksa.akhir] << endl;
+		xc::xy(kolom[r], baris[0]);
+		cout << header[r];
 	}
+
+	//perulangan yg digunakan untuk menggambar garis mendatar
+	for(auto i = kolom[0]-1; i <= kolom[2] + 51; ++i){
+		xc::xy(i, baris[0] - 1);
+		cout << char(196);
+		xc::xy(i, baris[0] + 1);
+		cout << char(196);
+		xc::xy(i, baris[1] - 1);
+		cout << char(196);
+		xc::xy(i, baris[1] + 1);
+		cout << char(196);
+		
+
+		//perulangan untuk menggambar garis ke bawah di bagian tengah
+		for(auto j = baris[0]-1; j <= baris[1] - 1; ++j){
+			if(j==4){
+				xc::xy(kolom[1]-1, j);
+				cout << char(194);
+				// xc::xy(kolom[2]-1, j);
+				// cout << char(194);
+				// xc::xy(kolom[3]-1, j);
+				// cout << char(194);
+				// xc::xy(kolom[4]-1, j);
+				// cout << char(194);
+			}
+			else if(j==6){
+				xc::xy(kolom[1]-1, j);
+				cout << char(197);
+				// xc::xy(kolom[2]-1, j);
+				// cout << char(197);
+				// xc::xy(kolom[3]-1, j);
+				// cout << char(197);
+				// xc::xy(kolom[4]-1, j);
+				// cout << char(197);
+			}
+			else if(j==24){
+				xc::xy(kolom[1]-1, j);
+				cout << char(193);
+				// xc::xy(kolom[2]-1, j);
+				// cout << char(193);
+				// xc::xy(kolom[3]-1, j);
+				// cout << char(193);
+				// xc::xy(kolom[4]-1, j);
+				// cout << char(193);
+			}
+			else{
+				xc::xy(kolom[1]-1, j);
+				cout << char(179);
+				// xc::xy(kolom[2]-1, j);
+				// cout << char(179);
+				// xc::xy(kolom[3]-1, j);
+				// cout << char(179);
+				// xc::xy(kolom[4]-1, j);
+				// cout << char(179);
+			}
+		}
+
+		//perulangan untuk menggambar garis ke bawah 
+		//di bagian sisi kanan dan kiri
+		for(auto k = baris[0]-1; k <= baris[1] + 1; ++k){
+			if(k==4){
+				xc::xy(kolom[0]-1, k);
+				cout << char(218);
+				xc::xy(kolom[4]+50, k);
+				cout << char(191);
+			}
+			else if(k==6){
+				xc::xy(kolom[0]-1, k);
+				cout << char(195);
+				xc::xy(kolom[4]+50, k);
+				cout << char(180);
+			}
+			else if(k==24){
+				xc::xy(kolom[0]-1, k);
+				cout << char(195);
+				xc::xy(kolom[4]+50, k);
+				cout << char(180);
+			}
+			else if(k==26){
+				xc::xy(kolom[0]-1, k);
+				cout << char(192);
+				xc::xy(kolom[4]+50, k);
+				cout << char(217);
+			}
+			else{
+				xc::xy(kolom[0]-1, k);
+				cout << char(179);
+				xc::xy(kolom[4]+50, k);
+				cout << char(179);
+			}
+		}
+		xc::xy(25, 27);cout << "Selesai";
+		
+	}
+
+	// for (auto i = 0; i < 2; ++i)
+	// {
+	// 	// xc::xy(col[i], isi);
+	// 	// cout << nomer;
+	// 	int nomer = 0;
+	// 	for (int j = kolom[22]+1; j <= kolom[22]-1; ++j)
+	// 	{
+	// 		xc::xy(kolom[22]+1, j);
+	// 		cout << nomer+1;
+	// 		xc::xy(kolom[1], j);
+	// 		cout << psn.nama[nomer];
+	// 		// xc::xy(kolom[2], j);
+	// 		// cout << psn.gender[nomer];
+	// 		// xc::xy(kolom[3], j);
+	// 		// cout << psn.umur[nomer];
+	// 		// xc::xy(kolom[4], j);
+	// 		// cout << psn.alamat[nomer];
+	// 		nomer++;
+	// 	}
+		
+	// }
+
+	getch();
 }
 
 void proses_antrean(){
 	cout << "Proses";
 }
 
+
 int col[] = {25, 31, 51, 60, 72};
 int row[] = {5, 25};
+int isi = 7;
 
 void show_pasien(){
 	system("cls");
-	// xc::color(10, 0);
-	// xc::xy(25,3); cout << "Daftar Pasien : ";
-	// xc::color(7, 0);
-	// string header[] = {"No", "Nama Pasien", "Gender", "Umur", "Alamat"};
-	// for(auto i = 0; i < 5; ++i)
-	// {
-	// 	xc::xy(col[i], row[0]);
-	// 	cout << header[i];
-	// }
+	xc::color(10, 0);
+	xc::xy(25,3); cout << "Daftar Pasien : ";
+	xc::color(7, 0);
+	string header[] = {"No", "Nama Pasien", "Gender", "Umur", "Alamat"};
+	for(auto i = 0; i < 5; ++i)
+	{
+		xc::xy(col[i], row[0]);
+		cout << header[i];
+	}
 
-	// // perulangan yg digunakan untuk menggambar garis mendatar
-	// for(auto i = col[0]-1; i <= col[4] + 20; ++i){
-	// 	xc::xy(i, row[0] - 1);
-	// 	cout << char(196);
-	// 	xc::xy(i, row[0] + 1);
-	// 	cout << char(196);
-	// 	xc::xy(i, row[1] - 1);
-	// 	cout << char(196);
-	// 	xc::xy(i, row[1] + 1);
-	// 	cout << char(196);
+	//perulangan yg digunakan untuk menggambar garis mendatar
+	for(auto i = col[0]-1; i <= col[4] + 20; ++i){
+		xc::xy(i, row[0] - 1);
+		cout << char(196);
+		xc::xy(i, row[0] + 1);
+		cout << char(196);
+		xc::xy(i, row[1] - 1);
+		cout << char(196);
+		xc::xy(i, row[1] + 1);
+		cout << char(196);
 		
 
-	// 	// perulangan untuk menggambar garis ke bawah di bagian tengah
-	// 	for(auto j = row[0]-1; j <= row[1] - 1; ++j){
-	// 		if(j==4){
-	// 			xc::xy(col[1]-1, j);
-	// 			cout << char(194);
-	// 			xc::xy(col[2]-1, j);
-	// 			cout << char(194);
-	// 			xc::xy(col[3]-1, j);
-	// 			cout << char(194);
-	// 			xc::xy(col[4]-1, j);
-	// 			cout << char(194);
-	// 		}
-	// 		else if(j==6){
-	// 			xc::xy(col[1]-1, j);
-	// 			cout << char(197);
-	// 			xc::xy(col[2]-1, j);
-	// 			cout << char(197);
-	// 			xc::xy(col[3]-1, j);
-	// 			cout << char(197);
-	// 			xc::xy(col[4]-1, j);
-	// 			cout << char(197);
-	// 		}
-	// 		else if(j==24){
-	// 			xc::xy(col[1]-1, j);
-	// 			cout << char(193);
-	// 			xc::xy(col[2]-1, j);
-	// 			cout << char(193);
-	// 			xc::xy(col[3]-1, j);
-	// 			cout << char(193);
-	// 			xc::xy(col[4]-1, j);
-	// 			cout << char(193);
-	// 		}
-	// 		else{
-	// 			xc::xy(col[1]-1, j);
-	// 			cout << char(179);
-	// 			xc::xy(col[2]-1, j);
-	// 			cout << char(179);
-	// 			xc::xy(col[3]-1, j);
-	// 			cout << char(179);
-	// 			xc::xy(col[4]-1, j);
-	// 			cout << char(179);
-	// 		}
-	// 	}
+		//perulangan untuk menggambar garis ke bawah di bagian tengah
+		for(auto j = row[0]-1; j <= row[1] - 1; ++j){
+			if(j==4){
+				xc::xy(col[1]-1, j);
+				cout << char(194);
+				xc::xy(col[2]-1, j);
+				cout << char(194);
+				xc::xy(col[3]-1, j);
+				cout << char(194);
+				xc::xy(col[4]-1, j);
+				cout << char(194);
+			}
+			else if(j==6){
+				xc::xy(col[1]-1, j);
+				cout << char(197);
+				xc::xy(col[2]-1, j);
+				cout << char(197);
+				xc::xy(col[3]-1, j);
+				cout << char(197);
+				xc::xy(col[4]-1, j);
+				cout << char(197);
+			}
+			else if(j==24){
+				xc::xy(col[1]-1, j);
+				cout << char(193);
+				xc::xy(col[2]-1, j);
+				cout << char(193);
+				xc::xy(col[3]-1, j);
+				cout << char(193);
+				xc::xy(col[4]-1, j);
+				cout << char(193);
+			}
+			else{
+				xc::xy(col[1]-1, j);
+				cout << char(179);
+				xc::xy(col[2]-1, j);
+				cout << char(179);
+				xc::xy(col[3]-1, j);
+				cout << char(179);
+				xc::xy(col[4]-1, j);
+				cout << char(179);
+			}
+		}
 
-	// 	// perulangan untuk menggambar garis ke bawah 
-	// 	// di bagian sisi kanan dan kiri
-	// 	for(auto k = row[0]-1; k <= row[1] + 1; ++k){
-	// 		if(k==4){
-	// 			xc::xy(col[0]-1, k);
-	// 			cout << char(218);
-	// 			xc::xy(col[4]+20, k);
-	// 			cout << char(191);
-	// 		}
-	// 		else if(k==6){
-	// 			xc::xy(col[0]-1, k);
-	// 			cout << char(195);
-	// 			xc::xy(col[4]+20, k);
-	// 			cout << char(180);
-	// 		}
-	// 		else if(k==24){
-	// 			xc::xy(col[0]-1, k);
-	// 			cout << char(195);
-	// 			xc::xy(col[4]+20, k);
-	// 			cout << char(180);
-	// 		}
-	// 		else if(k==26){
-	// 			xc::xy(col[0]-1, k);
-	// 			cout << char(192);
-	// 			xc::xy(col[4]+20, k);
-	// 			cout << char(217);
-	// 		}
-	// 		else{
-	// 			xc::xy(col[0]-1, k);
-	// 			cout << char(179);
-	// 			xc::xy(col[4]+20, k);
-	// 			cout << char(179);
-	// 		}
-	// 	}
-	// 	// xc::xy(25, 27);cout << "Selesai";		
-	// }
+		//perulangan untuk menggambar garis ke bawah 
+		//di bagian sisi kanan dan kiri
+		for(auto k = row[0]-1; k <= row[1] + 1; ++k){
+			if(k==4){
+				xc::xy(col[0]-1, k);
+				cout << char(218);
+				xc::xy(col[4]+20, k);
+				cout << char(191);
+			}
+			else if(k==6){
+				xc::xy(col[0]-1, k);
+				cout << char(195);
+				xc::xy(col[4]+20, k);
+				cout << char(180);
+			}
+			else if(k==24){
+				xc::xy(col[0]-1, k);
+				cout << char(195);
+				xc::xy(col[4]+20, k);
+				cout << char(180);
+			}
+			else if(k==26){
+				xc::xy(col[0]-1, k);
+				cout << char(192);
+				xc::xy(col[4]+20, k);
+				cout << char(217);
+			}
+			else{
+				xc::xy(col[0]-1, k);
+				cout << char(179);
+				xc::xy(col[4]+20, k);
+				cout << char(179);
+			}
+		}
+		xc::xy(25, 27);cout << "Selesai";
+		
+	}
+	
+	
 
-	for (int i = 0; i < psn.count; ++i)
+	/*for (int i = 0; i < psn.count; ++i)
 	{
 		cout << "No KTP : " << psn.id[i] << endl;
 		cout << "Nama   : " << psn.nama[i] << endl;
 		cout << "Gender : " << psn.gender[i] << endl;
 		cout << "Umur   : " << psn.umur[i] << endl;
 		cout << "Alamat : " << psn.alamat[i] << endl;
+	} */
+
+	
+	for (auto i = 0; i < 5; ++i)
+	{
+		// xc::xy(col[i], isi);
+		// cout << nomer;
+		int nomer = 0;
+		for (int j = row[0]+2; j <= row[1]-2; ++j)
+		{
+			xc::xy(col[0]+1, j);
+			cout << nomer+1;
+			xc::xy(col[1], j);
+			cout << psn.nama[nomer];
+			xc::xy(col[2], j);
+			cout << psn.gender[nomer];
+			xc::xy(col[3], j);
+			cout << psn.umur[nomer];
+			xc::xy(col[4], j);
+			cout << psn.alamat[nomer];
+			nomer++;
+		}
+		
 	}
 	getch();
 }
